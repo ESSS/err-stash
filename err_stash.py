897f6208e95e8f333e183262583176884dbd0a03
Bruno Oliveira <bruno@esss.com.br>
2017-12-04 13:09:36 -0200

    First commit

---
+++ err_stash.py
@@ -0,0 +1,278 @@
+import argparse
+from collections import OrderedDict
+from configparser import ConfigParser
+from pathlib import Path
+
+import stashy
+
+
+class StashAPI:
+    def __init__(self, url, project, *, username, password):
+        self._stash = stashy.connect(url, username=username, password=password)
+        self._project = project
+        self._url = url
+
+
+    @property
+    def project(self):
+        return self._project
+
+
+    @property
+    def url(self):
+        return self._url
+
+
+    def fetch_repos(self):
+        return self._stash.projects[self._project].repos.list()
+
+
+    def fetch_branches(self, slug, filter_text):
+        return self._stash.projects[self._project].repos[slug].branches(filterText=filter_text)
+
+    def delete_branch(self, slug, branch):
+        return self._stash.projects[self._project].repos[slug].delete_branch(branch)
+
+    def fetch_pull_requests(self, slug):
+        return self._stash.projects[self._project].repos[slug].pull_requests.all()
+
+
+    def fetch_pull_request(self, slug, pr_id):
+        return self._stash.projects[self._project].repos[slug].pull_requests[pr_id]
+
+    def fetch_repo_commits(self, slug, until, since):
+        return self._stash.projects[self._project].repos[slug].commits(until, since)
+
+
+class MergePlan:
+    def __init__(self, slug):
+        self.slug = slug
+        self.branches = []
+        self.pull_requests = []
+
+
+def get_self_url(d):
+    return d['links']['self'][0]['href']
+
+
+def commits_text(commits):
+    plural = 's' if len(commits) != 1 else ''
+    return '{} commit{}'.format(len(commits), plural)
+
+
+class CheckError(Exception):
+    def __init__(self, lines):
+        if isinstance(lines, str):
+            lines = [lines]
+        super().__init__('\n'.join(lines))
+        self.lines = lines
+
+
+def create_plans(api, branch_text):
+    repos = api.fetch_repos()
+    plans = []
+    has_prs = False
+    for repo in repos:
+        slug = repo['slug']
+        branches = list(api.fetch_branches(slug, branch_text))
+        if branches:
+            plan = MergePlan(slug)
+            plans.append(plan)
+            plan.branches = branches
+            branch_ids = [x['id'] for x in plan.branches]
+            prs = list(api.fetch_pull_requests(slug))
+            for pr in prs:
+                has_prs = True
+                if pr['fromRef']['id'] in branch_ids:
+                    plan.pull_requests.append(pr)
+
+    if not plans:
+        raise CheckError('Could not find any branch with text `"{}"` in any repositories of the `"{}"` project.'.format(
+            branch_text,
+            api.project,
+        ))
+
+    if not has_prs:
+        raise CheckError('No PRs are open with text `"{}"`'.format(branch_text))
+    return plans
+
+
+def ensure_text_matches_unique_branch(plans, branch_text):
+    # check if any of the plans have matched more than one branch
+    error_lines = []
+    for plan in plans:
+        if len(plan.branches) > 1:
+            if not error_lines:
+                error_lines.append('More than one branch matches the text `"{}"`:'.format(branch_text))
+            names = ', '.join('`{}`'.format(x['displayId']) for x in plan.branches)
+            error_lines.append("* `{slug}`: {names}".format(slug=plan.slug, names=names))
+
+    if error_lines:
+        error_lines.append("Use a more complete text or remove one of the branches.")
+        raise CheckError(error_lines)
+
+    return plans[0].branches[0]['displayId']
+
+
+def ensure_unique_pull_requests(plans, from_branch_display_id):
+    error_lines = []
+    for plan in plans:
+        if len(plan.pull_requests) > 1:
+            if not error_lines:
+                error_lines.append('Multiples PRs for branch `{}` found:'.format(from_branch_display_id))
+            links = ['[PR#{id}]({url})'.format(id=x['id'], url=get_self_url(x)) for x in plan.pull_requests]
+            error_lines.append("* `{slug}`: {links}".format(slug=plan.slug, links=', '.join(links)))
+    if error_lines:
+        error_lines.append('Sorry you will have to sort that mess yourself. :wink:')
+        raise CheckError(error_lines)
+
+
+def ensure_pull_requests_target_same_branch(plans, from_branch_display_id):
+    # check that all PRs for the branch target the same "to" branch
+    result = None
+    multiple_target_branches = False
+    for plan in plans:
+        if plan.pull_requests:
+            assert len(plan.pull_requests) == 1
+            if result is None:
+                result = plan.pull_requests[0]['toRef']['id']
+            elif result != plan.pull_requests[0]['toRef']['id']:
+                multiple_target_branches = True
+                break
+
+    if multiple_target_branches:
+        error_lines = ['PRs in repositories for branch `{}` have different targets:'.format(from_branch_display_id)]
+        for plan in plans:
+            if plan.pull_requests:
+                assert len(plan.pull_requests) == 1
+                pr = plan.pull_requests[0]
+                error_lines.append('* `{slug}`: [PR#{id}]({url}) targets `{to_ref}`'.format(
+                                   slug=plan.slug, id=pr['id'], url=get_self_url(pr), to_ref=pr['toRef']['id']))
+
+        error_lines.append('Fix those PRs and try again.')
+        raise CheckError(error_lines)
+
+    return result
+
+
+def make_pr_link(api, slug, from_branch, to_branch):
+    from urllib.parse import urlencode
+    params = OrderedDict([('sourceBranch', from_branch), ('targetBranch', to_branch)])
+    base_url = '{url}/projects/{project}/repos/{slug}/compare/commits?'.format(url=api.url, project=api.project,
+                                                                               slug=slug)
+    return base_url + urlencode(params)
+
+
+def get_commits_about_to_be_merged_by_pull_requests(api, plans, from_branch, to_branch):
+    error_lines = []
+    result = []
+    for plan in plans:
+        commits = list(api.fetch_repo_commits(plan.slug, from_branch, to_branch))
+        if commits and not plan.pull_requests:
+            if not error_lines:
+                error_lines.append('These repositories have commits in `{}` but no PRs:'.format(from_branch))
+            pr_link = make_pr_link(api, plan.slug, from_branch, to_branch)
+            error_lines.append('`{slug}`: **{commits_text}** ([create PR]({pr_link}))'.format(
+                slug=plan.slug, commits_text=commits_text(commits), pr_link=pr_link))
+        if commits:
+            result.append((plan, commits))
+
+    if error_lines:
+        error_lines.append('You need to create PRs for your changes before merging this branch.')
+        raise CheckError(error_lines)
+
+    return result
+
+
+def ensure_no_conflicts(api, from_branch, plans):
+    error_lines = []
+    for plan in plans:
+        pr_data = plan.pull_requests[0]
+        pull_request = api.fetch_pull_request(plan.slug, pr_data['id'])
+        if not pull_request.can_merge():
+            if not error_lines:
+                error_lines.append('The PRs below for branch `{}` have conflicts:'.format(from_branch))
+            error_lines.append('* `{slug}`: [PR#{id}]({url}) **CONFLICTS**'.format(
+                slug=plan.slug, id=pr_data['id'], url=get_self_url(pr_data)))
+
+    if error_lines:
+        error_lines.append('Fix the conflicts and try again. :wink:')
+        raise CheckError(error_lines)
+
+
+def merge(url, project, username, password, branch_text, confirm):
+    api = StashAPI(url, project, username=username, password=password)
+
+    # b1 = 'fb-SSRL-3737-sourres-export-tool'
+    # b2 = 'refs/heads/master'
+    # for x in api._stash.projects['ESSS'].repos['souring'].commits(b1, b2):
+    #     #import pprint
+    #     #pprint.pprint(x)
+    #     print(x)
+    #     #break
+    # return
+
+    plans = create_plans(api, branch_text)
+    from_branch = ensure_text_matches_unique_branch(plans, branch_text)
+    ensure_unique_pull_requests(plans, from_branch)
+    to_branch_id = ensure_pull_requests_target_same_branch(plans, from_branch)
+    assert to_branch_id, 'No pull requests! What to do?'
+    plans_and_commits = get_commits_about_to_be_merged_by_pull_requests(api, plans, from_branch, to_branch_id)
+    ensure_no_conflicts(api, from_branch, [plan for (plan, _) in plans_and_commits])
+
+    yield 'Branch `{}` merged into `{}`! :white_check_mark:'.format(from_branch, to_branch_id)
+    shown = set()
+    for plan, commits in plans_and_commits:
+        pull_request = api.fetch_pull_request(plan.slug, plan.pull_requests[0]['id'])
+        if confirm:
+            # https://confluence.atlassian.com/bitbucketserverkb/bitbucket-server-rest-api-for-merging-pull-request-fails-792309002.html
+            pull_request.merge(version=plan.pull_requests[0]['version'])
+        yield ':white_check_mark: `{}` **{}**'.format(plan.slug, commits_text(commits))
+        shown.add(plan.slug)
+    other_plans = (p for p in plans if p.slug not in shown)
+    for plan in other_plans:
+        yield '`{}` - (no changes)'.format(plan.slug)
+
+    for plan in plans:
+        if confirm:
+            api.delete_branch(plan.slug, plan.branches[0]['id'])
+    repo_list = ['`{}`'.format(p.slug) for p in plans]
+    yield 'Branch deleted from repositories: {}'.format(', '.join(repo_list))
+    if not confirm:
+        yield '{x} dry-run {x}'.format(x='-' * 30)
+
+
+def main(args):
+    p = Path(__file__).parent.joinpath('default.ini')
+    if p.is_file():
+        config = ConfigParser()
+        config.read(str(p))
+
+        default_user = config['err-stash']['user']
+        default_password = config['err-stash']['password']
+    else:
+        default_user = None
+        default_password = None
+
+    parser = argparse.ArgumentParser(description='Merge multiples branches.')
+    parser.add_argument('-u', '--username', default=default_user)
+    parser.add_argument('-p', '--password', default=default_password)
+    parser.add_argument('--project', default='ESSS')
+    parser.add_argument('--confirm', default=False, action='store_true')
+    parser.add_argument('text')
+
+    options = parser.parse_args(args)
+    try:
+        lines = list(merge("https://eden.esss.com.br/stash", options.project, username=options.username,
+                           password=options.password, branch_text=options.text, confirm=options.confirm))
+        result = 0
+    except CheckError as e:
+        lines = e.lines
+        result = 4
+    print('\n'.join(lines))
+    return result
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv[1:]))
---
+++ test.py
@@ -0,0 +1,224 @@
+import re
+from collections import OrderedDict
+
+import pytest
+import stashy
+
+
+from err_stash import merge, StashAPI, CheckError
+
+
+class DummyPullRequest:
+
+    def __init__(self, can_merge):
+        self._can_merge = can_merge
+        self.merged_version = None
+
+    def can_merge(self):
+        return self._can_merge
+
+    def merge(self, version):
+        self.merged_version = version
+
+
+@pytest.fixture
+def mock_api(mocker):
+    mocker.patch.object(stashy, 'connect', autospec=True)
+    api = StashAPI('https://myserver.com/stash', 'PROJ', username='fry', password='PASSWORD123')
+    args, kwargs = stashy.connect.call_args
+    assert args == ('https://myserver.com/stash',)
+    assert kwargs == dict(username='fry', password='PASSWORD123')
+    assert api.project == 'PROJ'
+
+    repos = OrderedDict([
+        ('repo1', dict(slug='repo1')),
+        ('repo2', dict(slug='repo2')),
+        ('repo3', dict(slug='repo3')),
+    ])
+    repos['repo1']['branches'] = [
+        dict(id='refs/heads/fb-ASIM-81-network', displayId='fb-ASIM-81-network'),
+        dict(id='refs/heads/fb-SSRL-1890-py3', displayId='fb-SSRL-1890-py3'),
+    ]
+    repos['repo2']['branches'] = [
+        dict(id='refs/heads/fb-SSRL-1890-py3', displayId='fb-SSRL-1890-py3'),
+    ]
+    repos['repo3']['branches'] = [
+        dict(id='refs/heads/fb-ASIM-81-network', displayId='fb-ASIM-81-network'),
+        dict(id='refs/heads/fb-SSRL-1890-py3', displayId='fb-SSRL-1890-py3'),
+    ]
+
+    repos['repo3']['pull_requests'] = [dict(id='10',
+                                            fromRef=dict(id='refs/heads/fb-ASIM-81-network'),
+                                            toRef=dict(id='refs/heads/master'),
+                                            displayId='fb-ASIM-81-network',
+                                            links=make_link('url.com/for/10'),
+                                            version='10')]
+
+    repos['repo3']['pull_request'] = {
+        '10': DummyPullRequest(True),
+    }
+
+    repos['repo3']['commits'] = {
+        ('refs/heads/fb-ASIM-81-network', 'refs/heads/master'): ['A', 'B'],
+    }
+
+    mocker.patch.object(StashAPI, 'fetch_repos', autospec=True, return_value=repos.values())
+
+    def mock_fetch_branches(self, slug, filter_text):
+        return [x for x in repos[slug]['branches'] if filter_text in x['id']]
+
+    mocker.patch.object(StashAPI, 'fetch_branches', autospec=True, side_effect=mock_fetch_branches)
+
+    def mock_fetch_pull_requests(self, slug):
+        return repos[slug].get('pull_requests', [])
+
+    mocker.patch.object(StashAPI, 'fetch_pull_requests', autospec=True, side_effect=mock_fetch_pull_requests)
+
+    def mock_fetch_pull_request(self, slug, pr_id):
+        return repos[slug].get('pull_request', {}).get(pr_id)
+
+    mocker.patch.object(StashAPI, 'fetch_pull_request', autospec=True, side_effect=mock_fetch_pull_request)
+
+    def mock_fetch_repo_commits(self, slug, from_branch, to_branch):
+        for (i_from_branch, i_to_branch), commits in repos[slug].get('commits', {}).items():
+            if from_branch in i_from_branch and to_branch in i_to_branch:
+                return commits
+        return []
+
+    mocker.patch.object(StashAPI, 'fetch_repo_commits', autospec=True, side_effect=mock_fetch_repo_commits)
+
+    def mock_delete_branch(self, slug, branch):
+        branches = repos[slug]['branches']
+        for index, item in reversed(list(enumerate(branches))):
+            if branch in item['id']:
+                del branches[index]
+
+    mocker.patch.object(StashAPI, 'delete_branch', autospec=True, side_effect=mock_delete_branch)
+
+    return repos
+
+
+def call_merge(branch_text, matching_lines):
+    try:
+        lines = list(merge('https://myserver.com/stash', 'PROJ', username='fry', password='PASSWORD123',
+                           branch_text=branch_text, confirm=True))
+    except CheckError as e:
+        lines = e.lines
+    from _pytest.pytester import LineMatcher
+    matcher = LineMatcher(list(lines))
+    matcher.re_match_lines(matching_lines)
+
+
+def make_link(url):
+    return dict(self=[dict(href=url)])
+
+
+def test_duplicate_branches(mock_api, mocker):
+    mock_api['repo1']['branches'] = [dict(id='refs/heads/fb-ASIM-81-network', displayId='fb-ASIM-81-network'),
+                                     dict(id='refs/heads/fb-ASIM-81-network-test', displayId='fb-ASIM-81-network-test')]
+    mock_api['repo2']['branches'] = [dict(id='refs/heads/fb-ASIM-81-network')]
+    mock_api['repo3']['branches'] = [dict(id='refs/heads/fb-ASIM-81-network'), dict(id='refs/heads/fb-SSRL-1890-py3')]
+
+    call_merge('ASIM-81', [
+        r'More than one branch matches the text `"ASIM-81"`:',
+        r'\* `repo1`: `fb-ASIM-81-network`, `fb-ASIM-81-network-test`',
+        r'Use a more complete text.*',
+    ])
+
+
+def test_multiples_prs_for_same_branch(mock_api):
+    mock_api['repo1']['pull_requests'] = [dict(id='10',
+                                               fromRef=dict(id='refs/heads/fb-ASIM-81-network'),
+                                               toRef=dict(id='refs/heads/master'),
+                                               displayId='fb-ASIM-81-network', links=make_link('url.com/for/10')),
+                                          dict(id='12',
+                                               fromRef=dict(id='refs/heads/fb-ASIM-81-network'),
+                                               toRef=dict(id='refs/heads/master'),
+                                               displayId='fb-ASIM-81-network', links=make_link('url.com/for/12'))
+                                          ]
+
+    call_merge('ASIM-81', [
+        r'Multiples PRs for branch `fb-ASIM-81-network` found:',
+        r'\* `repo1`: \[PR#10\]\(url.com/for/10\), \[PR#12\]\(url.com/for/12\)',
+        r'Sorry you will have to sort that mess yourself. :wink:',
+    ])
+
+
+def test_prs_with_different_targets(mock_api):
+    mock_api['repo1']['pull_requests'] = [dict(id='10',
+                                               fromRef=dict(id='refs/heads/fb-ASIM-81-network'),
+                                               toRef=dict(id='refs/heads/features'),
+                                               displayId='fb-ASIM-81-network', links=make_link('url.com/for/10'))]
+    mock_api['repo3']['pull_requests'] = [dict(id='17',
+                                               fromRef=dict(id='refs/heads/fb-ASIM-81-network'),
+                                               toRef=dict(id='refs/heads/master'),
+                                               displayId='fb-ASIM-81-network', links=make_link('url.com/for/17'))]
+
+    call_merge('ASIM-81', [
+        r'PRs in repositories for branch `fb-ASIM-81-network` have different targets:',
+        r'\* `repo1`: \[PR#10\]\(url.com/for/10\) targets `refs/heads/features`',
+        r'\* `repo3`: \[PR#17\]\(url.com/for/17\) targets `refs/heads/master`',
+        r'Fix those PRs and try again.'
+    ])
+
+
+def test_branch_commits_without_pr(mock_api):
+    from_branch = 'refs/heads/fb-ASIM-81-network'
+    to_branch = 'refs/heads/master'
+    mock_api['repo3']['commits'] = {(from_branch, to_branch): ['A', 'B']}
+    mock_api['repo1']['commits'] = {(from_branch, to_branch): ['C']}
+    pr_link = re.escape(
+        'https://myserver.com/stash/projects/PROJ/repos/repo1/compare/commits?'
+        'sourceBranch=fb-ASIM-81-network&'
+        'targetBranch=refs%2Fheads%2Fmaster'
+    )
+    call_merge('ASIM-81', [
+        r'These repositories have commits in `fb-ASIM-81-network` but no PRs:',
+        r'`repo1`: \*\*1 commit\*\* \(\[create PR\]\({pr_link}\)\)'.format(pr_link=pr_link),
+        r'You need to create PRs for your changes before merging this branch.',
+    ])
+
+
+def test_merge_conflicts(mock_api):
+    mock_api['repo3']['pull_request'] = {
+        '10': DummyPullRequest(False),
+    }
+    call_merge('ASIM-81', [
+        r'The PRs below for branch `fb-ASIM-81-network` have conflicts:',
+        r'\* `repo3`: \[PR#10\]\(url.com/for/10\) \*\*CONFLICTS\*\*',
+        r'Fix the conflicts and try again. :wink:',
+    ])
+    assert mock_api['repo3']['pull_request']['10'].merged_version is None
+
+
+def test_merge_success(mock_api):
+    pull_request = mock_api['repo3']['pull_request']['10']
+    call_merge('ASIM-81', [
+        r'Branch `fb-ASIM-81-network` merged into `refs/heads/master`! :white_check_mark:',
+        r':white_check_mark: `repo3` \*\*2 commits\*\*',
+        r'`repo1` - (no changes)',
+        r'Branch deleted from repositories: `repo1`, `repo3`'
+    ])
+    assert pull_request.merged_version == '10'
+    assert mock_api['repo1']['branches'] == [
+        dict(id='refs/heads/fb-SSRL-1890-py3', displayId='fb-SSRL-1890-py3'),
+    ]
+    assert mock_api['repo2']['branches'] == [
+        dict(id='refs/heads/fb-SSRL-1890-py3', displayId='fb-SSRL-1890-py3'),
+    ]
+    assert mock_api['repo3']['branches'] == [
+        dict(id='refs/heads/fb-SSRL-1890-py3', displayId='fb-SSRL-1890-py3'),
+    ]
+
+
+def test_no_pull_requests(mock_api):
+    del mock_api['repo3']['pull_requests']
+    call_merge('ASIM-81', [
+        r'No PRs are open with text `"ASIM-81"`',
+    ])
+
+
+def test_no_matching_branch(mock_api):
+    call_merge('FOOBAR-81', [
+        r'Could not find any branch with text `"FOOBAR-81"` in any repositories of the `"PROJ"` project.',
+    ])
